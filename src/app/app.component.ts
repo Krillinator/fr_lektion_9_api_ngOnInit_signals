1b57896e357d2a9235a2a894e5f6ddd5b73df557
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2025-03-04 10:19:48 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'lektion_9';
+
+  // Prepare API
+  productData: any; // WHY ANY? What do we do to change this...
+
+  // Student student = new Student(name: "Benny", password: "123")
+  // TODO - private - scope
+  constructor(private httpClient: HttpClient) {}
+
+  // TODO - Personal: fix extension for intellisense
+  // On Creation of component
+  // ngOnInit = always executes ONCE
+  ngOnInit() {
+    console.log('Hello world :)');
+
+    this.httpClient
+      .get('https://fakestoreapi.com/products')
+      .subscribe((response) => {
+        this.productData = response;
+      });
+  }
 }
